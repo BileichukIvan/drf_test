f48f80d5a8fd95063aadcab96a79da1e54263e47
ivanb <bileichuk.ivan@gmail.com>
2024-10-03 18:18:25 +0300

    added tests for team app models, views, serializers

---
+++ team/tests/test_serializers.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from rest_framework.exceptions import ValidationError
+from team.models import Team
+from team.serializers import TeamSerializer
+
+
+class TeamSerializerTestCase(TestCase):
+    def setUp(self):
+        """Create a sample Team instance"""
+        self.team = Team.objects.create(name="Development Team")
+
+    def test_team_serialization(self):
+        """Test serialization (model -> JSON)"""
+        serializer = TeamSerializer(self.team)
+        expected_data = {
+            "id": self.team.id,
+            "name": "Development Team"
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_team_deserialization(self):
+        """Test deserialization (JSON -> model)"""
+        data = {
+            "name": "QA Team"
+        }
+        serializer = TeamSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        team = serializer.save()
+        self.assertEqual(team.name, "QA Team")
+
+    def test_team_deserialization_invalid_data(self):
+        """Test deserialization with invalid data"""
+        data = {
+            "name": ""
+        }
+        serializer = TeamSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        with self.assertRaises(ValidationError):
+            serializer.save()
