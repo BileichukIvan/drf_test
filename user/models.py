2c8a47a87429bcb010a75d1163a7f85bb578238f
ivanb <bileichuk.ivan@gmail.com>
2024-10-03 12:22:11 +0300

    added model User for user/models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from team.models import Team
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    teams = models.ManyToManyField(Team, related_name="members", blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
