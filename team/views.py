a1af818681cce4617156a12b5027743a59e38cd2
ivanb <bileichuk.ivan@gmail.com>
2024-10-03 14:26:57 +0300

    cleaned user\views.py & team\views.py from unused code

--- team/views.py
+++ team/views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets, mixins
-from rest_framework.response import Response
-from rest_framework import status
 from team.models import Team
 from team.serializers import TeamSerializer
 
--- user/views.py
+++ user/views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets, mixins
-from rest_framework.response import Response
-from rest_framework import status
 from user.models import User
 from user.serializers import UserSerializer
 
