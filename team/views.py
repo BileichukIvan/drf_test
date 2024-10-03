d03900d6086b73412f25b53a1e47afeb0d0c0802
ivanb <bileichuk.ivan@gmail.com>
2024-10-03 12:30:28 +0300

    added TeamViewSet to team/views.py

--- team/views.py
+++ team/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from team.models import Team
+from team.serializers import TeamSerializer
 
-# Create your views here.
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
